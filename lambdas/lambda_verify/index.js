00a5ed43157aaa3c846c5f1d129c9a2137a046a4
Weronika Włodarczyk <weronika.wlodarczyk@netguru.com>
2022-09-21 10:24:55 +0200

    Change lambda name

--- lambdas/lambda_verify/index.js
+++ lambdas/lambda_verify/index.js
@@ -35,7 +35,7 @@ exports.handler = async (event) => {
   const stackName = process.env.STACK_NAME.split('-').slice(0, 3).join('-')
   const funcs = await lambda.listFunctions().promise();
   const commandHandlerLambda = funcs.Functions.find(
-    l => (l.FunctionName.includes('CommandHandler') && l.FunctionName.includes(stackName))
+    l => (l.FunctionName.includes('HandlerFunction') && l.FunctionName.includes(stackName))
   )
 
   if (commandHandlerLambda) {
