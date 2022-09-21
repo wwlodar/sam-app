efaf206c16ef852798c3822b8096187a6b0496bd
Weronika Włodarczyk <weronika.wlodarczyk@netguru.com>
2022-09-21 15:51:31 +0200

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,36 +1,6 @@
 import boto3
 import botocore
-import pytest
-# Set "running_locally" flag if you are running the integration test locally
-running_locally = True
-
-if running_locally:
-
-    # Create Lambda SDK client to connect to appropriate Lambda endpoint
-    lambda_client = boto3.client('lambda',
-        region_name="eu-central-1",
-        endpoint_url="http://127.0.0.1:3001",
-        use_ssl=False,
-        verify=False,
-        config=botocore.client.Config(
-            signature_version=botocore.UNSIGNED,
-            read_timeout=1,
-            retries={'max_attempts': 0},
-        )
-    )
-else:
-    lambda_client = boto3.client('lambda')
-
-
-# Invoke your Lambda function as you normally usually do. The function will run
-# locally if it is configured to do so
-response = lambda_client.invoke(FunctionName="HelloWorldFunction")
-
-# Verify the response
-assert response == "Hello World"
-
-
-from proxy import app
+from lambdas import lambda_verify
 import json
 import os
 import pytest
@@ -60,7 +30,7 @@ class TestMethodNotAllowed:
     def test_405_get(self, event, mock_context):
         '''Test a GET event.'''
 
-        response = app.lambda_handler(json.loads(event), mock_context)
+        response = lambda_verify.lambda_handler(json.loads(event), mock_context)
         body = json.loads(response['body'])
 
         assert response['statusCode'] == 405
