5209b4e0cce31060f8cc433e84a599ec1b79d4d5
Weronika Włodarczyk <weronika.wlodarczyk@netguru.com>
2022-09-21 15:50:35 +0200

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 import boto3
 import botocore
-
+import pytest
 # Set "running_locally" flag if you are running the integration test locally
 running_locally = True
 
@@ -28,3 +28,46 @@ response = lambda_client.invoke(FunctionName="HelloWorldFunction")
 
 # Verify the response
 assert response == "Hello World"
+
+
+from proxy import app
+import json
+import os
+import pytest
+
+
+@pytest.fixture(scope='function')
+def mock_context(mocker):
+    mock_context = mocker.MagicMock()
+    mock_context.aws_request_id = '00000000-0000-1000-0000-000000000000'
+    return mock_context
+
+
+@pytest.fixture(scope='function')
+def event(request):
+    dir = '{0}/../../events'.format(os.path.dirname(os.path.abspath(__file__)))
+    name = request.param
+    path = '{0}/{1}'.format(dir, name)
+    with open(path) as f:
+        event = f.read()
+    return event
+
+
+class TestMethodNotAllowed:
+
+    @pytest.mark.parametrize(
+        'event', ['405-get.json'], indirect=True)
+    def test_405_get(self, event, mock_context):
+        '''Test a GET event.'''
+
+        response = app.lambda_handler(json.loads(event), mock_context)
+        body = json.loads(response['body'])
+
+        assert response['statusCode'] == 405
+        assert 'exception' in body
+        assert 'error_message' in body['exception']
+        assert body['exception']['error_message'] == 'Method not allowed.'
+        assert body['exception']['error_type'] == 'MethodNotAllowedException'
+        assert body['exception']['http_method'] == 'GET'
+
+    # Tests for PUT and DELETE also exist.
